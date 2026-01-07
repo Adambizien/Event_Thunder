03bc8e8a41b50dc8633b3b9c20de8a35183fddf1
Adambizien <bizienadam@outlook.com>
2026-01-07 12:38:04 +0100

    delete useless file

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -24,5 +25,6 @@ import { UsersModule } from './users/users.module';
     }),
     UsersModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
