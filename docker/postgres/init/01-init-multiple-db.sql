bd339114c46a7492ed6657d29896ea35342cf50c
Adambizien <bizienadam@outlook.com>
2026-01-30 14:24:31 +0100

    Update docker-compose and initialization script for multiple databases

---
+++ docker/postgres/init/01-init-multiple-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE event_thunder_users;
+CREATE DATABASE event_thunder_events;
+CREATE DATABASE event_thunder_billing;
+CREATE DATABASE event_thunder_comments;
+CREATE DATABASE event_thunder_posts;
